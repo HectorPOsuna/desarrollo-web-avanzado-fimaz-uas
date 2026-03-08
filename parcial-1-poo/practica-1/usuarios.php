1f3099dbff690ff9bc534368925cd8806340a033
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 15:37:04 -0700

    feat: implementacion de los metodos seters y geters.

--- parcial-1-poo/practica-1/usuarios.php
+++ parcial-1-poo/practica-1/usuarios.php
@@ -8,9 +8,22 @@
         public function __construct(){
 
         }
-
         public function __destruct(){
-            
+                
+        }
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function getCorreo(){
+            return $this->correo;
+        }
+        public function setCorreo($correo){
+            $this->correo = $correo;
         }
     }
 ?>
