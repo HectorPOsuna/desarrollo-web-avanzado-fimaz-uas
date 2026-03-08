b68c0feb6ca8fcb904afce3d66ef46504981d766
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 15:34:10 -0700

    feat: creacion de los metodos constructor y destructor.

--- parcial-1-poo/practica-1/usuarios.php
+++ parcial-1-poo/practica-1/usuarios.php
@@ -4,5 +4,13 @@
     class Usuario{
         private $nombre;
         private $correo;
+
+        public function __construct(){
+
+        }
+
+        public function __destruct(){
+            
+        }
     }
 ?>
