197b398d0e034755c18dad2075226f9c5b911c5d
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-09 03:09:17 -0700

    refactor: eliminacion de sets en la clase usuarios

--- parcial-1-poo/practica-4/clases/Usuario.php
+++ parcial-1-poo/practica-4/clases/Usuario.php
@@ -40,15 +40,9 @@
         public function getNombre(){
             return $this->nombre;
         }
-        public function setNombre($nombre){
-            $this->nombre = $nombre;
-        }
 
         public function getCorreo(){
             return $this->correo;
-        }
-        public function setCorreo($correo){
-            $this->correo = $correo;
         }
     }
 ?>
