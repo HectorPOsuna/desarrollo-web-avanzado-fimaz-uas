1d99ad68d016b5461a09fbd441beffa54391064a
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 23:22:50 -0700

    feat: Creacion de la clase administrador con todos su metodos y propiedades

---
+++ parcial-1-poo/practica-3/clases/Admin.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * Clase Admin que representa a un administrador del sistema, que hereda de la clase Usuario.
+     * 
+     * @author Hector Manuel Padilla Osuna
+     * @version 1.0.0
+     */
+    class Admin extends Usuario{
+        
+        /**
+         * El rol del administrador
+         * 
+         * @var string $rol
+         */
+        private $rol;
+
+        public function __construct()
+        {
+            parent::__construct();
+            $this->rol = "administrador";
+        }
+
+        public function setRol($rol){
+            $this->rol = $rol;
+        }
+        public function getRol(){
+            return $this->rol;
+        }
+    }
+?>
