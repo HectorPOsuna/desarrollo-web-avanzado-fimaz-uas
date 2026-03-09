b4e8a8671220d3e32f93906e766b271f8071c36b
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-09 02:00:48 -0700

    feat: implementacion de la clase Invitado y su propiedad empresa

---
+++ parcial-1-poo/practica-4/clases/Invitado.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+     * Clase Invitado que representa a un invitado del sistema, que hereda de la clase Usuario.
+     * 
+     * @author Hector Manuel Padilla Osuna
+     * @version 1.0.0
+     */
+    class Invitado extends Usuario{
+        
+        /**
+         * La empresa del invitado
+         * 
+         * @var string $empresa
+         */
+        private $empresa;
+
+        
+    }
+?>
