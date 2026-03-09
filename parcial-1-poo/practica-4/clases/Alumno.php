96364d667764d6dc6ca1262c9da1c7f4f2d347b7
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-09 01:52:55 -0700

    refactor: Herencia cambiada de Admin a Usuario en la clase Alumno

---
+++ parcial-1-poo/practica-4/clases/Alumno.php
@@ -0,0 +1,33 @@
+<?php
+    /**
+     * Clase Alumno que representa a un alumno del sistema, que hereda de la clase Admin.
+     * 
+     * @author Hector Manuel Padilla Osuna
+     * @version 1.0.0
+     */
+    class Alumno extends Usuario{
+        
+        /**
+         * La matrícula del alumno
+         * 
+         * @var string $matricula
+         */
+        private $matricula;
+        
+        public function __construct()
+        {
+            $this->matricula = "948524-36";
+        }
+
+        public function setMatricula($matricula){
+            $this->matricula = $matricula;
+        }
+        public function getMatricula(){
+            return $this->matricula;
+        }
+        public function getRol()
+        {
+            return "alumno";
+        }
+    }
+?>
