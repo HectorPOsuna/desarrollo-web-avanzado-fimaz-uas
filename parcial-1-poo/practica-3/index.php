311f8a535061a65aa353be8324cb935a498fd700
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-09 00:29:51 -0700

    fix: modificacion de la ruta en el autoloader

--- parcial-1-poo/practica-3/index.php
+++ parcial-1-poo/practica-3/index.php
@@ -1,18 +1,19 @@
 <?php
+
     spl_autoload_register(function ($clase){
-        $ruta = __DIR__ . "/" . str_replace("\\", "/", $clase) . ".php";
+        $ruta = __DIR__ . "/clases/" . str_replace("\\", "/", $clase) . ".php";
         
         if(file_exists($ruta)){
             require($ruta);
         }
     });
 
-    $admin = new Admin();
+    $adm = new Admin();
 
-    $admin->setNombre("Hector Manuel Padilla Osuna");
-    $admin->setCorreo("hpadilla@uas.edu.mx");
+    $adm->setNombre("Hector Manuel Padilla Osuna");
+    $adm->setCorreo("hpadilla@uas.edu.mx");
 
-    echo "Nombre: " . $admin->getNombre() . "<br>";
-    echo "Correo: " . $admin->getCorreo() . "<br>";
-    echo "Rol: " . $admin->getRol() . "<br>";
+    echo "Nombre: " . $adm->getNombre() . "<br>";
+    echo "Correo: " . $adm->getCorreo() . "<br>";
+    echo "Rol: " . $adm->getRol() . "<br>";
 ?>
