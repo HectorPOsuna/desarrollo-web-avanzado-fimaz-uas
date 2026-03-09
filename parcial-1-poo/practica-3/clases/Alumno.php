e434fe6ffc5b4601d7b3ae79fdeb66df49ed33a6
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 23:46:10 -0700

    docs: documentacion del codigo para su correcta comprension

--- parcial-1-poo/practica-3/clases/Alumno.php
+++ parcial-1-poo/practica-3/clases/Alumno.php
@@ -1,5 +1,17 @@
 <?php
+    /**
+     * Clase Alumno que representa a un alumno del sistema, que hereda de la clase Admin.
+     * 
+     * @author Hector Manuel Padilla Osuna
+     * @version 1.0.0
+     */
     class Alumno extends Admin{
+        
+        /**
+         * La matrícula del alumno
+         * 
+         * @var string $matricula
+         */
         private $matricula;
         
         public function __construct()
@@ -15,6 +27,11 @@
             return $this->matricula;
         }
 
+        /**
+         * Sobrescribe el método getRol de la clase Admin para devolver "alumno" en lugar de "administrador".
+         * 
+         * @return string El rol del alumno, que es "alumno".
+         */
         public function getRol()
         {
             $this->setRol("alumno");
