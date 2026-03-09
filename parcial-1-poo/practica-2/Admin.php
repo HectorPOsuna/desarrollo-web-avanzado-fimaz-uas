ff2a602a892cc1ada3331fe011ab82e1ae4eecab
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 21:13:45 -0700

    docs: agregacion a la documentacion la herencia

--- parcial-1-poo/practica-2/Admin.php
+++ parcial-1-poo/practica-2/Admin.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * Clase Admin que representa a un administrador del sistema
+     * Clase Admin que representa a un administrador del sistema, que hereda de la clase Usuario.
      * 
      * @author Hector Manuel Padilla Osuna
      * @version 1.0.0
