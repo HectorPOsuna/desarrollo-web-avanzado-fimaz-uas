8864353b4b70bd10628fae78bebccab818ab8ab2
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 23:31:53 -0700

    feat: agregacion de constructor a la clase alumno

--- parcial-1-poo/practica-3/clases/Alumno.php
+++ parcial-1-poo/practica-3/clases/Alumno.php
@@ -2,5 +2,10 @@
     class Alumno extends Admin{
         private $matricula;
         
+        public function __construct()
+        {
+            return parent::__construct();
+            $this->matricula = "";
+        }
     }
 ?>
