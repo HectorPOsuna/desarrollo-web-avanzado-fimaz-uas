e8e5a606bddca291aa1df47905eaa668fdaa2123
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 23:21:57 -0700

    refactor: eliminacion del echo en el destructor

---
+++ parcial-1-poo/practica-3/clases/Usuario.php
@@ -0,0 +1,44 @@
+<?php
+    /**
+     * Clase Usuario que representa a un usuario del sistema
+     * 
+     * @author Hector Manuel Padilla Osuna
+     * @version 1.0.0
+     */
+    class Usuario{
+        
+        /**
+         * El nombre del usuario
+         * 
+         * @var string $nombre
+         */
+        private $nombre;
+        
+        /**
+         * El correo del usuario
+         * 
+         * @var string $correo
+         */
+        private $correo;
+
+        public function __construct(){
+            $this->nombre = "";
+            $this->correo = "";
+        }
+        public function __destruct(){}
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function getCorreo(){
+            return $this->correo;
+        }
+        public function setCorreo($correo){
+            $this->correo = $correo;
+        }
+    }
+?>
