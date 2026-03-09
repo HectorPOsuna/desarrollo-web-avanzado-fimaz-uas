898c5e48a8c242ed12c7e69e94f41d6fd5b6ff86
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 17:54:49 -0700

    feat: implementacion de la clase Usuario e instanciamiento de la misma clase

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -7,5 +7,13 @@
         }
     });
 
+    use Parcial1\Practica1\Usuario;
 
+    $usuario = new Usuario();
+
+    $usuario->setNombre("Hector Manuel Padilla Osuna");
+    $usuario->setCorreo("gamerspy2003@gmail.com");
+
+    echo "Nombre: " . $usuario->getNombre() . "<br>";
+    echo "Correo: " . $usuario->getCorreo() . "<br>";
 ?>
