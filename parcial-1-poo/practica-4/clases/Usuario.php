983c8c37851932ed08ddb2b75059de5c5f7cca07
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-09 02:52:26 -0700

    refactor: cambio de propiedades de protegidas a protected para una mejor utilizacion

--- parcial-1-poo/practica-4/clases/Usuario.php
+++ parcial-1-poo/practica-4/clases/Usuario.php
@@ -12,14 +12,14 @@
          * 
          * @var string $nombre
          */
-        private $nombre;
+        protected $nombre;
         
         /**
          * El correo del usuario
          * 
          * @var string $correo
          */
-        private $correo;
+        protected $correo;
 
         /**
          * Constructor de la clase Usuario, que inicializa el nombre y correo del usuario.
@@ -28,10 +28,6 @@
          * @param string $correo El correo electrónico del usuario, debe tener un formato válido.
          */
         public function __construct($nombre, $correo){
-            if(empty($nombre)){
-                throw new Exception("El nombre del usuario no puede estar vacío.");
-            }
-
             if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
                 throw new Exception("El correo electrónico no tiene un formato válido.");
             }
