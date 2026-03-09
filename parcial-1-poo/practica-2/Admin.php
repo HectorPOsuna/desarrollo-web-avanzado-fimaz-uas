355b4864ba54ed3f7c789deec7d71c668a65f163
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-08 20:50:49 -0700

    feat: implementacion de los constructores y destructores.

--- parcial-1-poo/practica-2/Admin.php
+++ parcial-1-poo/practica-2/Admin.php
@@ -1,5 +1,17 @@
 <?php
     class Admin extends Usuario{
+        private $rol;
+
+        public function __construct()
+        {
+            parent::__construct();
+            $this->rol = "admininstrador";
+        }
+        public function __destruct()
+        {
+            return parent::__destruct();
+        }
+
         
     }
 ?>
