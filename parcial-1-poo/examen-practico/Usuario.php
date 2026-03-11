848c94eab10284dd42b92e317a8981e1f5aa6101
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-11 00:05:01 -0700

    Documentacion de la clase y sus parametros

--- parcial-1-poo/examen-practico/Usuario.php
+++ parcial-1-poo/examen-practico/Usuario.php
@@ -1,6 +1,26 @@
 <?php
+    /**
+     * Clase usuario que guarda el nombre y correo de los usuarios
+     * 
+     * Esta clase permite obtener los nombres y correo de los usuarios que guarden en ella
+     * 
+     * @author Hector Manuel Padilla Osuna
+     * @version 1.0.0
+     */
+    
     class Usuario{
+        /**
+         * Nombre del usuario
+         * 
+         * @var string
+         */
         protected $nombre;
+
+        /**
+         * Correo del usuario
+         * 
+         * @var string
+         */
         protected $correo;
 
         public function __construct($correo,$nombre)
