99e6bc76b101af0793c852f20016c0ca5b9b9b05
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-11 00:13:58 -0700

    Creacion del archivo Admin con su clase Admin con herencia desde la clase Usuario

---
+++ parcial-1-poo/examen-practico/Admin.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Clase administrador que asigna el rol de administrador
+     * 
+     * @author HectorPOsuna
+     * @version 1.0.0
+     */
+    class Admin extends Usuario{
+
+        
+    }
+?>
