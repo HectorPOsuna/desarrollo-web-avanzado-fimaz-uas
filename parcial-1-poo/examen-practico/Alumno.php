ff51af28063acbafe41e7c57d1c93110577a0e8f
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-11 01:06:42 -0700

    Documentacion de la clase alumno

--- parcial-1-poo/examen-practico/Alumno.php
+++ parcial-1-poo/examen-practico/Alumno.php
@@ -1,12 +1,23 @@
 <?php
+    /**
+     * Clase alumno que guarda la matricula del alumno y regresa el rol de alumno
+     * 
+     * @author HectorPOsuna
+     * @version 1.0.0
+     */
     class Alumno extends Usuario{
+        /**
+         * Matricula del alumno
+         * 
+         * @var string
+         */
         private $matricula;
 
         public function __construct($nombre,$correo,$matricula){
             parent::__construct($nombre,$correo);
             $this->matricula = $matricula;
         }
-
+        
         public function getMatricula(){
             return $this->matricula;
         }
