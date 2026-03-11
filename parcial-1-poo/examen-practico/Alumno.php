1bd31f25e3791b773cb1b1e0d3755152e083c78d
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-03-11 00:21:09 -0700

    Implementacion del constructor en la clase Alumno

--- parcial-1-poo/examen-practico/Alumno.php
+++ parcial-1-poo/examen-practico/Alumno.php
@@ -2,6 +2,10 @@
     class Alumno extends Usuario{
         private $matricula;
 
+        public function __construct($matricula){
+            $this->matricula = $matricula;
+        }
+
         
     }
 ?>
