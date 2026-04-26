b9f99977df4b40dba13007c7b0b133cb491d4cde
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-25 23:50:01 -0700

    fix: correccion de sintaxis en el archivo de Database.php

--- parcial-2-pdo/practica-1/config/Database.php
+++ parcial-2-pdo/practica-1/config/Database.php
@@ -6,13 +6,13 @@
         private $password = "";
         private $connection;
 
-        public function __construnct() {
+        public function __construct() {
             try {
                 $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
                 $this->connection = new PDO($dsn, $this->username, $this->password);
                 
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FECTCH_ASSOC);
+                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             }catch (PDOException $e){
                 die("Error de conexion: " . $e->getMessage());
             }
