dcd8c52e8f52b1e09400967b554e643bfb85751d
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-25 23:30:10 -0700

    feat: agregacion de la funcionalidad de eliminar

--- parcial-2-pdo/practica-1/index.php
+++ parcial-2-pdo/practica-1/index.php
@@ -6,4 +6,13 @@
     $productoEditar = null;
 
     $terminoBusqueda = isset($_GET['buscar']) ? trim($_GET['buscar']) : '';
+
+    if (isset($_GET['eliminar'])) {
+        $idEliminar = $_GET['eliminar'];
+        if ($controller->eliminar($idEliminar)) {
+            $mensaje = "Producto eliminado correctamente.";
+        } else {
+            $mensaje = "Error al eliminar el producto.";
+        }
+}
 ?>
