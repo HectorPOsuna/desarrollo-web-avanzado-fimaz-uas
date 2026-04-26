3f87e8b9c0b6db4b8b32bc3d9a423d6e249b085e
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-26 00:35:15 -0700

    feat: implementacion de la conexion a la base de datos mediante PDO

--- parcial-2-pdo/practica-3/index.php
+++ parcial-2-pdo/practica-3/index.php
@@ -11,4 +11,16 @@ $charset = "utf8mb4";
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
+/********************************
+ * CONEXIÓN PDO (con excepciones)
+ ********************************/
+try {
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // clave: errores como excepciones
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+
 ?>
