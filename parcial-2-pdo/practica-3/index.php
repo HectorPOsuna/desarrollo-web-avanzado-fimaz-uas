2412f4feeb1f97c96a76aa4052cf45b44d4e3da0
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-26 00:33:53 -0700

    feat: implementacion de los parametros para la conexion a la base de datos

---
+++ parcial-2-pdo/practica-3/index.php
@@ -0,0 +1,14 @@
+<?php
+/********************************
+* CONFIGURACIÓN
+********************************/
+
+$host    = "localhost";
+$db      = "dwebavanzado";
+$user    = "root";
+$pass    = "";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+?>
