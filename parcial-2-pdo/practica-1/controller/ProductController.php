f9f540dcc4d170cfb50886f91d704ba69c19d3d0
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-25 18:41:08 -0700

    feat: creacion de la clase ProductController con sus parametros y su metodo constructor

--- parcial-2-pdo/practica-1/controller/ProductController.php
+++ parcial-2-pdo/practica-1/controller/ProductController.php
@@ -1,4 +1,13 @@
 <?php
     require_once __DIR__ . '/../config/Database.php';
     require_once __DIR__ . '/../model/ProductModel.php';
+
+    class ProductController {
+        private $connection;
+
+        public function __construct() {
+            $database = new Database();
+            $this -> connection = $database -> getConnection();
+        }
+    }
 ?>
