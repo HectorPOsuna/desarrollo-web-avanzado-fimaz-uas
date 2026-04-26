3e4ec1b45b479f4f15359e77b69138326c7f4603
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-25 18:33:36 -0700

    feat: creacion del constructor de la clase ProductModel

--- parcial-2-pdo/practica-1/model/ProductModel.php
+++ parcial-2-pdo/practica-1/model/ProductModel.php
@@ -5,5 +5,13 @@
         private $descripcion;
         private $existencia;
         private $precio;
+
+        public function __construct($id = null, $nombre = "", $descripcion = "", $existencia = 0, $precio = 0.0){
+            $this->id = $id;
+            $this->nombre = $nombre;
+            $this->descripcion = $descripcion;
+            $this->existencia = $existencia;
+            $this->precio = $precio;
+        }
     }
 ?>
