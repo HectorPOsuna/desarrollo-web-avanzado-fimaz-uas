5482dc46ec4b4e34139b215445532eaec1b7542b
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-25 18:26:42 -0700

    feat: implementacion de la conexion a la base de datos con PDO

--- parcial-2-pdo/practica-1/config/Database.php
+++ parcial-2-pdo/practica-1/config/Database.php
@@ -5,5 +5,15 @@
         private $username = "root";
         private $password = "";
         private $connection;
+
+        public function __construnct() {
+            try {
+                $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
+                $this->connection = new PDO($dsn, $this->username, $this->password);
+                
+                $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FECTCH_ASSOC);
+            }catch
+        }
     }
 ?>
