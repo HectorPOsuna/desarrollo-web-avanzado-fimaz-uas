1a6477c8bcbe0a88baa2c65437380403fbe51f22
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-26 18:13:38 -0700

    feat: creacion de la clase Database con sus parametros.

--- parcial-2-pdo/examen-practico/config/Database.php
+++ parcial-2-pdo/examen-practico/config/Database.php
@@ -1,6 +1,12 @@
 <?php
 namespace examen\Config;
 
-
+class Database {
+    private $host = "localhost";
+    private $dbname = "dwebavanzado";
+    private $username = "root";
+    private $password = "";
+    private $connection;
+}
 
 ?>
