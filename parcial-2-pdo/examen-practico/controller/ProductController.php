ca6ed22fc8ead7e1251612eb3fec7f19168bdc72
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-26 20:42:51 -0700

    feat: implementacion de la funcion crear con metodos try/catch

--- parcial-2-pdo/examen-practico/controller/ProductController.php
+++ parcial-2-pdo/examen-practico/controller/ProductController.php
@@ -11,5 +11,24 @@ class ProductController {
         $database = new Database();
         $this->connection = $database->getConnection();
     }
+
+    public function crear(Producto $producto) {
+        try {
+            $sql = "INSERT INTO productos (nombre, descripcion, existencia, precio)
+                    VALUES (:nombre, :descripcion, :existencia, :precio)";
+
+            $stmt = $this->connection->prepare($sql);
+
+            $stmt->bindValue(':nombre', $producto->getNombre());
+            $stmt->bindValue(':descripcion', $producto->getDescripcion());
+            $stmt->bindValue(':existencia', $producto->getExistencia(), \PDO::PARAM_INT);
+            $stmt->bindValue(':precio', $producto->getPrecio());
+
+            return $stmt->execute();
+
+        } catch (PDOException $e) {
+            die("Error al crear el producto: " . $e->getMessage());
+        }
+    }
 }
 ?>
