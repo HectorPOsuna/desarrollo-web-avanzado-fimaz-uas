7ce62003044f586b6d297c85079d262b1f27b473
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-04-26 20:41:19 -0700

    feat: implementacion de la clase ProductController y su constructor

--- parcial-2-pdo/examen-practico/controller/ProductController.php
+++ parcial-2-pdo/examen-practico/controller/ProductController.php
@@ -4,5 +4,12 @@ namespace App\Controller;
 use App\Model\ProductModel;
 use App\Config\Database;
 
-
+class ProductController {
+    private $connection;
+    
+    public function __construct() {
+        $database = new Database();
+        $this->connection = $database->getConnection();
+    }
+}
 ?>
