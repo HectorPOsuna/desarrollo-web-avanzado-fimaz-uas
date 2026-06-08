7085e06be08bd413c579581146db653eecfe88b9
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-06-07 22:33:19 -0700

    feat: implementacion de la funcion generar en Csrf.php

--- proyecto-final/helpers/Csrf.php
+++ proyecto-final/helpers/Csrf.php
@@ -2,6 +2,15 @@
 namespace Helpers;
 class Csrf
 {
-    
+    public static function generar(): string
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        if (empty($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['csrf_token'];
+    }
 }
 ?>
