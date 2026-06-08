8d5afef023bad77737d3405464bd69f6cd33e901
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-06-07 22:25:46 -0700

    docs: documentacion del codigo autoload

--- proyecto-final/config/Autoload.php
+++ proyecto-final/config/Autoload.php
@@ -1,14 +1,22 @@
 <?php
-    spl_autoload_register(function ($class) {
-        $baseDir = __DIR__ . '/../';
-        $class = str_replace('\\', '/', $class);
-        $parts = explode('/', $class);
-        if (!empty($parts)) {
-            $parts[0] = strtolower($parts[0]);
-        }
-        $file = $baseDir . implode('/', $parts) . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-        }
-    });
-?>
+/**
+ * Registra el autoloader de clases del proyecto.
+ *
+ * Convierte el namespace en una ruta de archivo y lo carga automaticamente.
+ *
+ * @package Config
+ * @author Hector Manuel Padilla Osuna
+ * @version 1.0.0
+ */
+spl_autoload_register(function ($class) {
+    $baseDir = __DIR__ . '/../';
+    $class = str_replace('\\', '/', $class);
+    $parts = explode('/', $class);
+    if (!empty($parts)) {
+        $parts[0] = strtolower($parts[0]);
+    }
+    $file = $baseDir . implode('/', $parts) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
