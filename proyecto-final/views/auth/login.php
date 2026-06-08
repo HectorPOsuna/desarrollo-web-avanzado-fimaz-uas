dce27c3dced6a35ab6235db130468fa62d2e045d
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-06-07 23:22:55 -0700

    feat: implementacion de la autenticacion CSRF

--- proyecto-final/views/auth/login.php
+++ proyecto-final/views/auth/login.php
@@ -1,4 +1,5 @@
 <?php require_once __DIR__ . '/../layouts/header.php'; ?>
+<?php use Helpers\Csrf; ?>
 
 <div class="row justify-content-center">
     <div class="col-md-5">
@@ -7,7 +8,8 @@
                 Iniciar sesion
             </div>
             <div class="card-body">
-                <form action="index.php?route=auth/login" method="POST">
+                <form action="<?= BASE_URL ?>auth/login" method="POST">
+                    <?= Csrf::campo(); ?>
                     <div class="mb-3">
                         <label class="form-label">Usuario</label>
                         <input type="text" name="username" class="form-control" required>
