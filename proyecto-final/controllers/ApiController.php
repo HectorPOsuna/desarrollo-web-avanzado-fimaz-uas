8b3c30d450e8e117037b762f2d7807e08602b062
Hector Manuel Padilla Osuna <gamerspy2003@gmail.com>
2026-06-07 23:00:01 -0700

    feat: implementacion del namespace y la clase ApiController en ApiController.php

---
+++ proyecto-final/controllers/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Controllers;
+
+use Models\ProductoModel;
+
+/**
+ * Clase que gestiona la API REST de productos.
+ *
+ * @package Controllers
+ * @author Hector Manuel Padilla Osuna
+ * @version 1.0.0
+ */
+class ApiController
+{
+    
+}
